810163b563aff33b7f8127bd0e29dc5bbddc4986
Tim Holmes <tim@gravitaz.co.uk>
2020-01-06 15:51:14 +0000

    add more debug to find out why client not starting

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log('React client process running');
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
