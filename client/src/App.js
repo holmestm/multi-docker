d0ee9eb00a731e6f25bb79bad4c529dcf4f5e518
Tim Holmes <tim@gravitaz.co.uk>
2020-01-07 13:02:45 +0000

    Change app title

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ import Fib from './Fib';
 
 class App extends Component {
   render() {
+    console.log('React client process running');
     return (
       <Router>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator version 2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
